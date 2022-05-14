be1d519eaff018a0a7888899c1b05966f8635e94
NataGabbriel <oliveiranataninha10@gmail.com>
2022-05-14 01:36:22 -0300

    commit

--- main.py
+++ main.py
@@ -10,3 +10,22 @@ dicionario = ast.literal_eval(base_dados)
 dicio_norma = funcoes.normaliza(dicionario)
 
 repost = 's'
+
+
+while repost != "s":
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        if repost != "n":
+        
+            repost = input('Caractere Inválido! Você deseja jogar de novo(s/n):')
+        else:
+            break
